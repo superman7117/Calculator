ed2841fb80edcbdffc0857a1e812f41d83f6e46b
Clark Ozanne <clarkozanne@gamil.com>
2016-01-07 21:37:10 -0800

    numbers on screen

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ console.log('working')
 
 
 document.addEventListener('DOMContentLoaded', function (){
-var currentTotal = 0;
+var currentDisplay = 0;
 var currentOperator = '';
 var totalHistory = [];
 var lastInput = 0;
-
-function numClicked(event){
-  var button = event.target;
+var catHolder = [];
+var numberButtons = document.getElementsByClassName('num');
+for(var i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', numberClicked);
 }
+function numberClicked(event) {
+  var button = event.target.innerHTML;
+  catHolder.push(button);
+  currentDisplay = catHolder.join('')
+document.getElementById('display').innerHTML = currentDisplay;
+}
+
   // document.getElementById('boxx').addEventListener('click', function(){
   //   console.log("yes");
   //   var box =document.getElementById('boxx')
