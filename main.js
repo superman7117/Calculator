90227d1a5da8b2ccec359b320c713eb1e0d9a61d
Clark Ozanne <clarkozanne@gamil.com>
2016-01-08 08:37:11 -0800

    Start History

--- main.js
+++ main.js
@@ -18,19 +18,69 @@ function numberClicked(event) {
   currentDisplay = catHolder.join('')
 document.getElementById('display').innerHTML = currentDisplay;
 }
+var operatorButtons = document.getElementsByClassName('operator');
+for(var i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', operatorClicked);
+}
+function operatorClicked(event) {
+  var button = event.target.innerHTML;
+  switch (button) {
+    case '=':
+    catHolder = [];
+    var answer = parseFloat(lastInput)+currentOperator+parseFloat(currentDisplay);
 
-  // document.getElementById('boxx').addEventListener('click', function(){
-  //   console.log("yes");
-  //   var box =document.getElementById('boxx')
-  //   if (box.style.backgroundColor !== 'lime') {
-  //     box.style.backgroundColor = 'lime';
-  //     box.className = 'box2';
-  //   }
-  //   else {
-  //     box.style.backgroundColor = 'blue';
-  //   box.className = 'box';
-  // }
-  // })
-
-
+    currentDisplay= eval(answer);
+    totalHistory.push(currentDisplay);
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case '+':
+    lastInput = parseFloat(currentDisplay);
+    currentOperator = '+';
+    catHolder = [];
+    currentDisplay = 0;
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case '-':
+    lastInput = parseFloat(currentDisplay);
+    currentOperator = '-';
+    catHolder = [];
+    currentDisplay = 0;
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case 'X':
+    lastInput = parseFloat(currentDisplay);
+     currentOperator = '*';
+    catHolder = [];
+    currentDisplay = 0;
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case '/':
+    lastInput = parseFloat(currentDisplay);
+    currentOperator = '/';
+    catHolder = [];
+    currentDisplay = 0;
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case '%':
+        currentDisplay = parseFloat(currentDisplay)/100;
+        document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case '+ or -':
+    currentDisplay = parseFloat(currentDisplay)*-1;
+    document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case 'C':
+        catHolder = [];
+        currentDisplay = 0;
+        document.getElementById('display').innerHTML = currentDisplay;
+        break;
+    case 'History':
+    console.log(totalHistory);
+        catHolder = [];
+        for (var i = 0; i < totalHistory.length; i ++) {
+          currentDisplay = totalHistory[i + 1];
+        }
+        document.getElementById('display').innerHTML = currentDisplay;
+}
+}
 })
