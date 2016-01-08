1dc405dff7d7ce74838855de2145af456be1afe5
Clark Ozanne <clarkozanne@gamil.com>
2016-01-07 20:06:01 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict'
+console.log('working')
+
+
+document.addEventListener('DOMContentLoaded', function (){
+var currentTotal = 0;
+var currentOperator = '';
+var totalHistory = [];
+var lastInput = 0;
+
+function numClicked(event){
+  var button = event.target;
+}
+  // document.getElementById('boxx').addEventListener('click', function(){
+  //   console.log("yes");
+  //   var box =document.getElementById('boxx')
+  //   if (box.style.backgroundColor !== 'lime') {
+  //     box.style.backgroundColor = 'lime';
+  //     box.className = 'box2';
+  //   }
+  //   else {
+  //     box.style.backgroundColor = 'blue';
+  //   box.className = 'box';
+  // }
+  // })
+
+
+})
